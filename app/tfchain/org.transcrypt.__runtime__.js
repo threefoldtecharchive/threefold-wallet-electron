83ef7317c8ed76652a55736dd5653ba6767611ed
Glen De Cauwsemaecker <contact@glendc.com>
2019-05-18 20:50:27 +0200

    add blake2b support to the polyfill.crypto module

--- app/tfchain/org.transcrypt.__runtime__.js
+++ app/tfchain/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-05-18 19:22:00
+// Transcrypt'ed from Python, 2019-05-18 20:49:54
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
