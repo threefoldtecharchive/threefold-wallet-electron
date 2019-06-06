df31101f3e52c9572c983fea6cbc93d4d794f00b
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-06-06 13:34:50 +0200

    some refactor and new icons

--- app/menu.js
+++ app/menu.js
@@ -1,49 +1,47 @@
 // @flow
-import { app, Menu, shell, BrowserWindow } from 'electron';
+import { app, Menu, shell } from 'electron'
 
 export default class MenuBuilder {
-  mainWindow: BrowserWindow;
-
-  constructor(mainWindow: BrowserWindow) {
-    this.mainWindow = mainWindow;
+  constructor (mainWindow) {
+    this.mainWindow = mainWindow
   }
 
-  buildMenu() {
+  buildMenu () {
     if (
       process.env.NODE_ENV === 'development' ||
       process.env.DEBUG_PROD === 'true'
     ) {
-      this.setupDevelopmentEnvironment();
+      this.setupDevelopmentEnvironment()
     }
 
     const template =
       process.platform === 'darwin'
         ? this.buildDarwinTemplate()
-        : this.buildDefaultTemplate();
+        : this.buildDefaultTemplate()
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 
-    return menu;
+    return menu
   }
 
-  setupDevelopmentEnvironment() {
-    this.mainWindow.openDevTools();
+  setupDevelopmentEnvironment () {
+    this.mainWindow.openDevTools()
     this.mainWindow.webContents.on('context-menu', (e, props) => {
-      const { x, y } = props;
+      const { x, y } = props
 
       Menu.buildFromTemplate([
         {
           label: 'Inspect element',
           click: () => {
-            this.mainWindow.inspectElement(x, y);
+            this.mainWindow.inspectElement(x, y)
           }
         }
-      ]).popup(this.mainWindow);
-    });
+      ]).popup(this.mainWindow)
+    })
   }
 
-  buildDarwinTemplate() {
+  buildDarwinTemplate () {
     const subMenuAbout = {
       label: 'Tfchain Wallet',
       submenu: [
@@ -52,7 +50,6 @@ export default class MenuBuilder {
           selector: 'orderFrontStandardAboutPanel:'
         },
         { type: 'separator' },
-        { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
           label: 'Hide Tfchain Wallet',
@@ -70,11 +67,11 @@ export default class MenuBuilder {
           label: 'Quit',
           accelerator: 'Command+Q',
           click: () => {
-            app.quit();
+            app.quit()
           }
         }
       ]
-    };
+    }
     const subMenuEdit = {
       label: 'Edit',
       submenu: [
@@ -90,7 +87,7 @@ export default class MenuBuilder {
           selector: 'selectAll:'
         }
       ]
-    };
+    }
     const subMenuViewDev = {
       label: 'View',
       submenu: [
@@ -98,25 +95,25 @@ export default class MenuBuilder {
           label: 'Reload',
           accelerator: 'Command+R',
           click: () => {
-            this.mainWindow.webContents.reload();
+            this.mainWindow.webContents.reload()
           }
         },
         {
           label: 'Toggle Full Screen',
           accelerator: 'Ctrl+Command+F',
           click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen())
           }
         },
         {
           label: 'Toggle Developer Tools',
           accelerator: 'Alt+Command+I',
           click: () => {
-            this.mainWindow.toggleDevTools();
+            this.mainWindow.toggleDevTools()
           }
         }
       ]
-    };
+    }
     const subMenuViewProd = {
       label: 'View',
       submenu: [
@@ -124,11 +121,11 @@ export default class MenuBuilder {
           label: 'Toggle Full Screen',
           accelerator: 'Ctrl+Command+F',
           click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen())
           }
         }
       ]
-    };
+    }
     const subMenuWindow = {
       label: 'Window',
       submenu: [
@@ -141,46 +138,34 @@ export default class MenuBuilder {
         { type: 'separator' },
         { label: 'Bring All to Front', selector: 'arrangeInFront:' }
       ]
-    };
+    }
     const subMenuHelp = {
       label: 'Help',
       submenu: [
         {
-          label: 'Learn More',
-          click() {
-            shell.openExternal('http://electron.atom.io');
-          }
-        },
-        {
           label: 'Documentation',
-          click() {
+          click () {
             shell.openExternal(
-              'https://github.com/atom/electron/tree/master/docs#readme'
-            );
-          }
-        },
-        {
-          label: 'Community Discussions',
-          click() {
-            shell.openExternal('https://discuss.atom.io/c/electron');
+              'https//github.com/threefoldfoundation/tchain-wallet-electron/README.md'
+            )
           }
         },
         {
           label: 'Search Issues',
-          click() {
-            shell.openExternal('https://github.com/atom/electron/issues');
+          click () {
+            shell.openExternal('https//github.com/threefoldfoundation/tchain-wallet-electron')
           }
         }
       ]
-    };
+    }
 
     const subMenuView =
-      process.env.NODE_ENV === 'development' ? subMenuViewDev : subMenuViewProd;
+      process.env.NODE_ENV === 'development' ? subMenuViewDev : subMenuViewProd
 
-    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
+    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp]
   }
 
-  buildDefaultTemplate() {
+  buildDefaultTemplate () {
     const templateDefault = [
       {
         label: '&File',
@@ -193,7 +178,7 @@ export default class MenuBuilder {
             label: '&Close',
             accelerator: 'Ctrl+W',
             click: () => {
-              this.mainWindow.close();
+              this.mainWindow.close()
             }
           }
         ]
@@ -203,75 +188,63 @@ export default class MenuBuilder {
         submenu:
           process.env.NODE_ENV === 'development'
             ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  }
-                },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  }
-                },
-                {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.toggleDevTools();
-                  }
+              {
+                label: '&Reload',
+                accelerator: 'Ctrl+R',
+                click: () => {
+                  this.mainWindow.webContents.reload()
                 }
-              ]
+              },
+              {
+                label: 'Toggle &Full Screen',
+                accelerator: 'F11',
+                click: () => {
+                  this.mainWindow.setFullScreen(
+                    !this.mainWindow.isFullScreen()
+                  )
+                }
+              },
+              {
+                label: 'Toggle &Developer Tools',
+                accelerator: 'Alt+Ctrl+I',
+                click: () => {
+                  this.mainWindow.toggleDevTools()
+                }
+              }
+            ]
             : [
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  }
+              {
+                label: 'Toggle &Full Screen',
+                accelerator: 'F11',
+                click: () => {
+                  this.mainWindow.setFullScreen(
+                    !this.mainWindow.isFullScreen()
+                  )
                 }
-              ]
+              }
+            ]
       },
       {
         label: 'Help',
         submenu: [
           {
-            label: 'Learn More',
-            click() {
-              shell.openExternal('http://electron.atom.io');
-            }
-          },
-          {
             label: 'Documentation',
-            click() {
+            click () {
               shell.openExternal(
-                'https://github.com/atom/electron/tree/master/docs#readme'
-              );
-            }
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://discuss.atom.io/c/electron');
+                'https//github.com/threefoldfoundation/tchain-wallet-electron/README.md'
+              )
             }
           },
           {
             label: 'Search Issues',
-            click() {
-              shell.openExternal('https://github.com/atom/electron/issues');
+            click () {
+              shell.openExternal('https//github.com/threefoldfoundation/tchain-wallet-electron')
             }
           }
         ]
       }
-    ];
+    ]
 
-    return templateDefault;
+    return templateDefault
   }
 }
