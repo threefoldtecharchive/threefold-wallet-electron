841fcb522a5eb1fa92e96ef9087c1fc4c78816e9
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-06-06 11:27:06 +0200

    added multisig wallet transaction on transfer page, closes #56

--- app/components/Transfer.js
+++ app/components/Transfer.js
@@ -1,7 +1,7 @@
 // @flow
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
-import { Form, Button, Input, Icon, Dropdown, Divider, Loader, Dimmer, Message } from 'semantic-ui-react'
+import { Form, Button, Input, Icon, Dropdown, Divider, Loader, Dimmer, Message, Radio, Popup } from 'semantic-ui-react'
 import styles from './home/Home.css'
 import Footer from './footer'
 import { toast } from 'react-toastify'
@@ -9,6 +9,7 @@ import { selectWallet, setBalance, setTransactionJson } from '../actions'
 import * as tfchain from '../tfchain/api'
 import moment from 'moment'
 import routes from '../constants/routes'
+import { filter } from 'lodash'
 
 const mapStateToProps = state => ({
   account: state.account,
@@ -43,7 +44,12 @@ class Transfer extends Component {
       selectedWallet: this.props.account.wallets[0].wallet_name,
       loader: false,
       datelock: '',
-      timelock: ''
+      timelock: '',
+      multiSigTransaction: false,
+      signatureCount: 2,
+      ownerAddressErrors: [false, false],
+      ownerAddresses: ['', ''],
+      signatureCountError: false
     }
   }
 
@@ -83,6 +89,130 @@ class Transfer extends Component {
     this.setState({ amount: target.value })
   }
 
+  handleMultiSigTransactionCheck = () => {
+    const newState = !this.state.multiSigTransaction
+    this.setState({ multiSigTransaction: newState })
+  }
+
+  handleSignatureCountChange = ({ target }) => {
+    const { ownerAddresses } = this.state
+    if (target.value > ownerAddresses.length) {
+      this.setState({ signatureCountError: true })
+    } else {
+      this.setState({ signatureCountError: false })
+    }
+    this.setState({ signatureCount: target.value })
+  }
+
+  renderSignatureCountError = () => {
+    const { signatureCountError } = this.state
+    if (signatureCountError) {
+      return (
+        <Message negative>
+          <p style={{ fontSize: 12 }}>Signature count must be less than or equal to the number of owners.</p>
+        </Message>
+      )
+    }
+  }
+
+  addOwnerAddress = () => {
+    const { ownerAddresses } = this.state
+    ownerAddresses.push('')
+    this.setState({ ownerAddresses, signatureCount: ownerAddresses.length })
+  }
+
+  removeOwnerAddress = (index) => {
+    const { ownerAddresses } = this.state
+    ownerAddresses.splice(index, 1)
+    this.setState({ ownerAddresses, signatureCount: ownerAddresses.length })
+  }
+
+  handleAddressOwnerChange = (e, index) => {
+    const { ownerAddresses, ownerAddressErrors } = this.state
+    const { target } = e
+    if (!tfchain.wallet_address_is_valid(target.value) && target.value !== '') {
+      ownerAddressErrors.splice(index, 1)
+      ownerAddressErrors.insert(index, true)
+      this.setState({ ownerAddressErrors })
+    } else {
+      ownerAddressErrors.splice(index, 1)
+      ownerAddressErrors.insert(index, false)
+      this.setState({ ownerAddressErrors })
+    }
+    const newOwnerAddresses = ownerAddresses[index] = target.value
+    this.setState({ ownerAddress: newOwnerAddresses })
+  }
+
+  renderOwnerInputFields = () => {
+    const { ownerAddresses, ownerAddressErrors } = this.state
+    return ownerAddresses.map((owner, index) => {
+      return (
+        <div key={index} >
+          <div style={{ display: 'flex', marginTop: 10 }}>
+            <Input
+              error={ownerAddressErrors[index]}
+              style={{ background: '#0c111d !important', color: '#7784a9' }}
+              icon={<Icon name='user' style={{ color: '#0e72f5' }} />}
+              iconPosition='left'
+              placeholder='owner address'
+              value={owner}
+              onChange={(e) => this.handleAddressOwnerChange(e, index)}
+            />
+            {ownerAddresses.length > 2
+              ? (<Icon name='trash' onClick={() => this.removeOwnerAddress(owner, index)} style={{ fontSize: 20, position: 'relative', top: 10, marginLeft: 20 }} />)
+              : (null)}
+          </div>
+          {ownerAddressErrors[index]
+            ? (
+              <Message negative>
+                <Message.Header style={{ fontSize: 16, height: '50%' }}>Invalid address</Message.Header>
+              </Message>
+            )
+            : (null)
+          }
+        </div>
+      )
+    })
+  }
+
+  renderDestinationForm = () => {
+    const { multiSigTransaction, destinationError, destination, signatureCount } = this.state
+    if (!multiSigTransaction) {
+      return (
+        <Form.Field style={{ marginTop: 10 }}>
+          <Input
+            error={destinationError}
+            style={{ background: '#0c111d !important', color: '#7784a9' }}
+            icon={<Icon name='send' style={{ color: '#0e72f5' }} />}
+            iconPosition='left'
+            placeholder='destination address'
+            value={destination}
+            onChange={this.handleDestinationChange}
+          />
+          {this.renderDestinationError()}
+        </Form.Field>
+      )
+    } else {
+      return (
+        <Form.Field style={{ marginTop: 10 }}>
+          <Form.Field >
+            {this.renderOwnerInputFields()}
+            <Icon name='plus circle' style={{ fontSize: 30, marginTop: 20, cursor: 'pointer', position: 'relative', left: 290 }} onClick={() => this.addOwnerAddress()} />
+          </Form.Field>
+          <Form.Field>
+            <div style={{ display: 'flex' }}>
+              <label style={{ float: 'lft', color: 'white' }}>Signature count</label>
+              <Popup offset={0} size='large' position='right center' content='Signature count is the count of signatures that this multsig wallet requires to send tansactions.' trigger={<Icon style={{ fontSize: 12, float: 'left', marginLeft: 10 }} name='question circle' />} />
+            </div>
+            <Input style={{ marginTop: 10 }} type='number' placeholder='1' min='0' value={signatureCount} onChange={this.handleSignatureCountChange} />
+            {this.renderSignatureCountError()}
+          </Form.Field>
+          {this.renderDestinationError()}
+        </Form.Field>
+      )
+    }
+  }
+
   mapWalletsToDropdownOption = () => {
     const { wallets } = this.props.account
     return wallets.map(w => {
@@ -120,7 +250,21 @@ class Transfer extends Component {
   }
 
   submitTransaction = () => {
-    const { description, destination, amount, selectedWallet, amountError, datelock, timelock } = this.state
+    const {
+      description,
+      destination,
+      amount,
+      selectedWallet,
+      amountError,
+      datelock,
+      timelock,
+      multiSigTransaction,
+      ownerAddressErrors,
+      ownerAddresses,
+      signatureCount,
+      signatureCountError
+    } = this.state
+
     let destinationError = false
     // let descriptionError = false
 
@@ -139,6 +283,14 @@ class Transfer extends Component {
     //   descriptionError = true
     // }
 
+    if (!multiSigTransaction) {
+      this.buildSingleTransaction(destinationError, destination, amountError, selectedWallet, timestamp, amount, description)
+    } else {
+      this.buildMultiSignTransaction(amountError, selectedWallet, ownerAddresses, ownerAddressErrors, signatureCount, signatureCountError, timestamp, amount, description)
+    }
+  }
+
+  buildSingleTransaction = (destinationError, destination, amountError, selectedWallet, timestamp, amount, description) => {
     if (!destinationError && !amountError && selectedWallet != null) {
       this.renderLoader(true)
       const selectedWalletFromProps = this.props.account.wallets.filter(w => w.wallet_name === selectedWallet)[0]
@@ -163,6 +315,53 @@ class Transfer extends Component {
         this.props.setBalance(this.props.account)
         if (result.submitted) {
           toast('Transaction ' + result.transaction.id + ' submitted')
+          return this.goBack()
+        } else {
+          this.props.setTransactionJson(JSON.stringify(result.transaction.json()))
+          return this.props.history.push(routes.SIGN)
+        }
+      }).catch(err => {
+        toast('transaction failed')
+        this.setState({ loader: false, errorMessage: err.__str__() })
+      })
+    } else {
+      toast.error('All fields are required !')
+    }
+  }
+
+  buildMultiSignTransaction = (amountError, selectedWallet, ownerAddresses, ownerAddressErrors, signatureCount, signatureCountError, timestamp, amount, description) => {
+    const hasOwnerAddressErrors = ownerAddressErrors.filter(e => e === true).length > 0
+    const areAllOwnersFilledIn = filter(ownerAddresses, o => o === '').length === 0
+
+    if (signatureCount < ownerAddresses.length) {
+      signatureCountError = true
+      this.setState({ signatureCountError: true })
+    }
+
+    if (!signatureCountError && !amountError && selectedWallet != null && !hasOwnerAddressErrors && areAllOwnersFilledIn) {
+      this.renderLoader(true)
+      const selectedWalletFromProps = this.props.account.wallets.filter(w => w.wallet_name === selectedWallet)[0]
+      const builder = selectedWalletFromProps.transaction_new()
+      if (timestamp) {
+        try {
+          builder.output_add([signatureCount, ownerAddresses], amount.toString(), { lock: timestamp })
+        } catch (error) {
+          toast('transaction failed')
+          return this.setState({ loader: false, errorMessage: error.__str__() })
+        }
+      } else {
+        try {
+          builder.output_add([signatureCount, ownerAddresses], amount.toString())
+        } catch (error) {
+          toast('transaction failed')
+          return this.setState({ loader: false, errorMessage: error.__str__() })
+        }
+      }
+      builder.send({ data: description }).then(result => {
+        this.setState({ ownerAddressErrors, amountError, loader: false })
+        this.props.setBalance(this.props.account)
+        if (result.submitted) {
+          toast('Multisign Transaction ' + result.transaction.id + ' submitted')
           return this.goBack()
         } else {
           this.props.setTransactionJson(JSON.stringify(result.transaction.json()))
@@ -218,7 +417,7 @@ class Transfer extends Component {
   }
 
   render () {
-    const { destination, destinationError, amountError, amount, timelock, datelock, selectedWallet } = this.state
+    const { amountError, amount, timelock, datelock, selectedWallet } = this.state
     const walletsOptions = this.mapWalletsToDropdownOption()
 
     if (this.state.loader) {
@@ -237,12 +436,25 @@ class Transfer extends Component {
         <Divider style={{ background: '#1A253F' }} />
         <Icon onClick={() => this.props.history.goBack()} style={{ fontSize: 25, marginLeft: 15, marginTop: 15, cursor: 'pointer' }} name='chevron circle left' />
         <span onClick={() => this.props.history.goBack()} style={{ width: 60, fontFamily: 'SF UI Text Light', fontSize: 12, cursor: 'pointer', position: 'relative', top: -5 }}>Go Back</span>
-        <Form error style={{ width: '50%', marginLeft: '20%', marginTop: 50 }}>
+        <Form error style={{ width: '50%', marginLeft: '20%', marginTop: 10, overflowY: 'auto', height: 500, padding: 30 }}>
           <h2 style={{ marginBottom: 20 }}>Send funds to:</h2>
-          <Form.Field style={{ marginTop: 10 }}>
-            <Input error={destinationError} style={{ background: '#0c111d !important', color: '#7784a9' }} icon={<Icon name='send' style={{ color: '#0e72f5' }} />} iconPosition='left' placeholder='destination address' value={destination} onChange={this.handleDestinationChange} />
-            {this.renderDestinationError()}
+          <Form.Field>
+            <Radio
+              label={<label style={{ color: 'white' }}>Wallet</label>}
+              value={this.state.multiSigTransaction}
+              checked={!this.state.multiSigTransaction}
+              onChange={this.handleMultiSigTransactionCheck}
+            />
           </Form.Field>
+          <Form.Field>
+            <Radio
+              label={<label style={{ color: 'white' }}>Multisignature Wallet</label>}
+              value={this.state.multiSigTransaction}
+              checked={this.state.multiSigTransaction}
+              onChange={this.handleMultiSigTransactionCheck}
+            />
+          </Form.Field>
+          {this.renderDestinationForm()}
           <Form.Field style={{ marginTop: 30 }}>
             <Input type='number' error={amountError} label='Amount TFT' style={{ background: '#0c111d !important', color: '#7784a9', width: 150 }} placeholder='amount' value={amount} onChange={this.handleAmountChange} />
             {this.renderAmountError()}
@@ -265,7 +477,7 @@ class Transfer extends Component {
           </Form.Field>
           {this.renderErrorMessage()}
         </Form>
-        <div style={{ position: 'absolute', bottom: 150, right: 80 }}>
+        <div style={{ position: 'absolute', bottom: 110, right: 50 }}>
           <Button className={styles.cancelButton} onClick={() => this.props.history.goBack()} style={{ marginTop: 20, float: 'left', background: '#2B3C72', color: 'white', marginRight: 15 }} size='big'>Cancel</Button>
           <Button className={styles.acceptButton} onClick={() => this.submitTransaction()} style={{ marginTop: 20, marginRight: 10, float: 'left', background: '#015DE1', color: 'white' }} size='big'>Send</Button>
         </div>
