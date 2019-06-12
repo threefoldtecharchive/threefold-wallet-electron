ba5003b84eb0dfd189ef5eb4342aadab638c38c2
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-06-12 10:00:19 +0200

    reduced number of last known locations in store

--- app/reducers/router.js
+++ app/reducers/router.js
@@ -1,7 +1,11 @@
+import { takeRight } from 'lodash'
+
 export const routerLocations = (state = [], action) => {
   switch (action.type) {
     case '@@router/LOCATION_CHANGE':
-      return [...state, action.payload]
+      // only save last 9 locations + last one = 10.
+      const slicedLocations = takeRight(state, 9)
+      return [...slicedLocations, action.payload]
     case 'RESET_APP':
       return []
     default:
