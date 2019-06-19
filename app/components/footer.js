85933ea4138bd0e4d2718cab8361f97977c42ebf
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-06-19 09:01:17 +0200

    fixed position of footer, closes #134

--- app/components/footer.js
+++ app/components/footer.js
@@ -43,7 +43,7 @@ class Footer extends Component {
     const tz = momentTz.tz.guess()
 
     return (
-      <div style={{ position: 'absolute', height: 70, bottom: 0, width: '100%', background: '#131216', borderTopStyle: 'solid', borderTopWidth: 2, borderTopColor: '#1A253F', padding: 25 }}>
+      <div style={{ zIndex: 999, position: 'absolute', height: 70, bottom: 0, width: '100%', background: '#131216', borderTopStyle: 'solid', borderTopWidth: 2, borderTopColor: '#1A253F', padding: 25 }}>
         {error || !chainConstants.chain_network
           ? <div>
             <Icon name='circle' style={{ color: 'red', marginLeft: 10 }} />
