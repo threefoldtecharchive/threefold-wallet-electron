d67c9134b7edb35812829df17f7da2146a601fae
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-06-26 19:14:28 +0200

    restyled wallet page balance, closes #152

--- app/components/wallet/BalanceGrid.js
+++ app/components/wallet/BalanceGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Segment, Icon } from 'semantic-ui-react'
+import { Segment, Icon, Grid } from 'semantic-ui-react'
+import styles from '../home/Home.css'
 
 const WalletBalanceGrid = ({ walletBalance }) => {
   const {
@@ -12,23 +13,28 @@ const WalletBalanceGrid = ({ walletBalance }) => {
   } = walletBalance
 
   return (
-    <Segment style={{ background: 'linear-gradient(90deg, rgba(51,55,186,1) 0%, rgba(71,122,254,1) 100%)', margin: 'auto', marginTop: 0, height: '25vh', width: '90%', border: 'none' }}>
-      <div>
-        <div style={{ display: 'flex' }}>
-          <div style={{ width: '50%' }}>
-            <h3 style={{ color: 'white', marginTop: 0 }}>Total Balance:</h3>
-            <h4 style={{ color: 'white', marginTop: 0 }}>{coinsTotal.str()} TFT</h4>
-            {unconfirmedTotalCoins.greater_than(0) ? (<span style={{ color: 'white', marginTop: 0, fontSize: 12 }}>* unconfirmed: {unconfirmedTotalCoins.str({ precision: 3 })} TFT</span>) : (<p />)}
-          </div>
-          <div style={{ width: '50%' }}>
-            <h4 style={{ color: 'white' }}><Icon name='lock' />Locked Balance:</h4>
-            <h4 style={{ color: 'white', marginTop: 0 }}>{coinsLocked.str()} TFT</h4>
-            {unconfirmedLockedCoins.greater_than(0) ? (<span style={{ color: 'white', marginTop: 0, fontSize: 12 }}>* unconfirmed: {unconfirmedLockedCoins.str({ precision: 3 })} TFT</span>) : (<p />)}
-            <h4 style={{ color: 'white' }}><Icon name='unlock' />Unlocked Balance: </h4>
-            <h4 style={{ color: 'white', marginTop: 0 }}>{coinsUnlocked.str()} TFT</h4>
-            {unconfirmedUnlockedCoins.greater_than(0) ? (<span style={{ color: 'white', marginTop: 0, fontSize: 12 }}>* unconfirmed: {unconfirmedUnlockedCoins.str({ precision: 3 })} TFT </span>) : (<p />)}
-          </div>
-        </div>
+    <Segment style={{ background: '#131421', border: 'none', paddingTop: 0, paddingLeft: 0, paddingRight: 0, width: '90%', margin: 'auto' }}>
+      <div style={{ background: 'linear-gradient(90deg, rgba(102,60,198,1) 0%, rgba(169,92,202,1) 100%)', width: '100%', textAlign: 'center', height: 50, paddingTop: 15, borderRadius: 2 }}>
+        <p style={{ fontFamily: 'SF UI Text Bold', fontSize: 16, textTransform: 'uppercase' }}>Wallet Balance</p>
+      </div>
+      <div div style={{ padding: 20 }}>
+        <Grid columns='3'>
+          <Grid.Column style={{ textAlign: 'center' }}>
+            <h4 style={{ marginTop: 0 }}>Total Balance</h4>
+            <h4 className={styles.gradientTitle} >{coinsTotal.str({ precision: 3 })} TFT</h4>
+            {unconfirmedTotalCoins.greater_than(0) ? (<span style={{ color: 'white', fontSize: 12 }}>* unconfirmed: {unconfirmedTotalCoins.str({ precision: 3 })} TFT</span>) : (<p />)}
+          </Grid.Column>
+          <Grid.Column style={{ textAlign: 'center' }}>
+            <h4><Icon name='lock' />Locked Balance</h4>
+            <h4>{coinsLocked.str({ precision: 3 })}  TFT</h4>
+            {unconfirmedLockedCoins.greater_than(0) ? (<span style={{ color: 'white', fontSize: 12 }}>* unconfirmed: {unconfirmedLockedCoins.str({ precision: 3 })} TFT</span>) : (<p />)}
+          </Grid.Column>
+          <Grid.Column style={{ textAlign: 'center' }}>
+            <h4><Icon name='unlock' />Unlocked Balance</h4>
+            <h4 style={{ marginBottom: 0 }}>{coinsUnlocked.str({ precision: 3 })}  TFT</h4>
+            {unconfirmedUnlockedCoins.greater_than(0) ? (<span style={{ color: 'white', fontSize: 12 }}>* unconfirmed: {unconfirmedUnlockedCoins.str({ precision: 3 })} TFT </span>) : (<p />)}
+          </Grid.Column>
+        </Grid>
       </div>
     </Segment>
   )
