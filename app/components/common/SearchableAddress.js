285602e108380eadbd023193614fc94dec4ca56b
Dean Verjans <dean.verjans@student.odisee.be>
2019-07-03 11:16:12 +0200

    Added click on searchableAddress to show the available options
    also replaced key variable name back to original

--- app/components/common/SearchableAddress.js
+++ app/components/common/SearchableAddress.js
@@ -58,33 +58,50 @@ class SearchableAddress extends Component {
 
   handleSearchChange = (e, { value }) => {
     this.setState({ value })
+    this.filterSearchInput()
+  }
 
-    setTimeout(() => {
-      // if (this.state.value.length < 1) return this.setState(initialState)
-      if (!tfchain.wallet_address_is_valid(value)) {
-        this.setState({ addressError: true })
-      } else {
-        this.setState({ addressError: false })
-      }
+  filterSearchInput = () => {
+    const value = this.state.value
 
-      const re = new RegExp(escapeRegExp(this.state.value), 'i')
+    this.setState({ addressError: !tfchain.wallet_address_is_valid(value) })
 
-      // enable searching on name or address
-      const isMatch = result => (re.test(result.wallet_name) || re.test(result.value) || re.test(result.title) || re.test(result.contact_name))
-      const results = filter(this.state.source, isMatch)
+    const re = new RegExp(escapeRegExp(this.state.value), 'i')
 
-      this.props.setSearchValue(value)
-      // If no results, this means the user copied or typed an address that he does not know yet.
-      // Pass this address
-      if (results.length === 0) {
-        return this.setState({ value, results, showNoResults: false })
-      }
+    // enable searching on name or address
+    const isMatch = result => (re.test(result.wallet_name) || re.test(result.value) || re.test(result.title) || re.test(result.contact_name))
+    const results = filter(this.state.source, isMatch)
 
-      // If results are found, show a dropdown list with possible selection
-      this.setState({
-        results: filter(this.state.source, isMatch)
-      })
-    }, 50)
+    this.props.setSearchValue(value)
+
+    // If no results, this means the user copied or typed an address that he does not know yet.
+    // Pass this address
+    if (results.length === 0) {
+      return this.setState({ results, showNoResults: false })
+    }
+
+    // If results are found, show a dropdown list with possible selection
+    this.setState({
+      results: results
+    })
+  }
+
+  handleOnFocus = (e) => {
+    const { value, source } = this.state
+
+    if (value !== undefined) {
+      return this.filterSearchInput()
+    }
+
+    this.setState({
+      showNoResults: false, results: source
+    })
+  }
+
+  handleOnBlur = () => {
+    this.setState({
+      showNoResults: true, results: []
+    })
   }
 
   render () {
@@ -95,13 +112,16 @@ class SearchableAddress extends Component {
         <Search
           style={this.props.style}
           onResultSelect={this.handleResultSelect}
-          onSearchChange={this.props.disabled === false ? null : debounce(this.handleSearchChange, 500, {
+          onFocus={this.handleOnFocus}
+          onBlur={this.handleOnBlur}
+          onSearchChange={debounce(this.handleSearchChange, 100, {
             leading: true
           })}
           results={results}
           value={value || this.props.value}
           placeholder='address'
           showNoResults={showNoResults}
+          minCharacters={0}
           icon={<Icon name={this.props.icon} position='left' style={{ color: '#0e72f5' }} />}
         />
         {addressError ? (
