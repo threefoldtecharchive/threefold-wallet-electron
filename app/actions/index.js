c0f90814bccd1d54bf2136b38aa0e8fc869b44f5
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-07-16 14:40:55 +0200

    fixed dispatching notifications

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,4 +1,5 @@
 import { toast } from 'react-toastify'
+import { store } from '../store/configureStore'
 let blockId
 
 export const addAccount = function (account) {
@@ -37,48 +38,39 @@ export const selectAccount = function (account) {
 }
 
 export const getTransactionsNotifications = function (account) {
-  console.log('---getting notifications---', account)
-
-  console.log('doing something?')
-  return dispatch => {
-    const { chain_info: chaininfo } = account
-    account.wallets.map(w => {
-      const block = chaininfo.last_block_get({
-        addresses: w.addresses
-      })
-      console.log('saved blockid: ', blockId)
-      console.log('blockid from tx: ', block.identifier)
-      if (blockId !== block.identifier) {
-        if (block.transactions.length > 0) {
-          console.log('wallet with name: ', w.wallet_name)
-          console.log('--txs---', block.transactions)
-          block.transactions.forEach(tx => {
-            if (tx.inputs.length > 0) {
-              dispatch({
-                type: 'INCREASE_NOTIFICATION_COUNT',
-                title: 'Transaction',
-                description: 'Incoming transaction received'
-              })
-              toast('Incoming transaction received')
-            }
-            if (tx.outputs.length > 0) {
-              dispatch({
-                type: 'INCREASE_NOTIFICATION_COUNT',
-                title: 'Transaction',
-                description: 'Outgoing transaction received'
-              })
-              toast('Outgoing transaction received')
-            }
-          })
-        }
+  const { chain_info: chaininfo } = account
+  account.wallets.map(w => {
+    const block = chaininfo.last_block_get({
+      addresses: w.addresses
+    })
+    if (blockId !== block.identifier) {
+      if (block.transactions.length > 0) {
+        block.transactions.forEach(tx => {
+          if (tx.inputs.length > 0) {
+            store.dispatch({
+              type: 'INCREASE_NOTIFICATION_COUNT',
+              title: 'Transaction',
+              description: 'Incoming transaction received'
+            })
+            toast('Incoming transaction received')
+          }
+          if (tx.outputs.length > 0) {
+            store.dispatch({
+              type: 'INCREASE_NOTIFICATION_COUNT',
+              title: 'Transaction',
+              description: 'Outgoing transaction received'
+            })
+            toast('Outgoing transaction received')
+          }
+        })
       }
-      blockId = block.identifier
-    })
-    dispatch({
-      type: 'GET_TX_FOR_WALLET',
-      tx: null
-    })
-  }
+    }
+    blockId = block.identifier
+  })
+  store.dispatch({
+    type: 'GET_TX_FOR_WALLET',
+    tx: null
+  })
 }
 
 export const resetApp = function () {
