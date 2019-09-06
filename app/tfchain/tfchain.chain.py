ac8887352feab010864931053e7a79bb34193212
Glen De Cauwsemaecker <contact@glendc.com>
2019-09-06 15:43:34 +0200

    support tfbchain and fftchain
    closes #260

--- app/tfchain/tfchain.chain.py
+++ app/tfchain/tfchain.chain.py
@@ -162,6 +162,138 @@ class GoldChainNetworkType(NetworkType):
 GoldChainNetworkType.TESTNET = GoldChainNetworkType(1)
 GoldChainNetworkType.DEVNET = GoldChainNetworkType(2)
 
+
+class ThreefoldBonusNetworkType(NetworkType):
+    def __init__(self, value):
+        if isinstance(value, ThreefoldBonusNetworkType):
+            value = value.value
+        elif isinstance(value, str):
+            value = ThreefoldBonusNetworkType.from_str(value).value
+        if not isinstance(value, int):
+            raise TypeError("ThreeFoldBonus network type value was expected to be an int, not be of type {}".format(type(value)))
+        if value < 1 or value > 2:
+            raise ValueError("ThreeFoldBonus network type out of range: {}".format(value))
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def __eq__(self, other):
+        if isinstance(other, ThreefoldBonusNetworkType):
+            return self.value == other.value
+        return self.value == other
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __int__(self):
+        return self.value
+
+    def __str__(self):
+        if self.__eq__(ThreefoldBonusNetworkType.TESTNET):
+            return "testnet"
+        if self.__eq__(ThreefoldBonusNetworkType.DEVNET):
+            return "devnet"
+        raise ValueError("invalid ThreeFoldBonus network type {}".format(self.value))
+
+    @classmethod
+    def from_str(cls, s):
+        if not isinstance(s, str):
+            raise TypeError("can only convert from a string")
+        s = jsstr.lower(s)
+        if s in ("testnet", "test"):
+            return ThreefoldBonusNetworkType.TESTNET
+        if s in ("devnet", "dev"):
+            return ThreefoldBonusNetworkType.DEVNET
+        raise ValueError(s + " is not a supported ThreeFoldBonus network type str")
+
+    @classmethod
+    def default_network_type(cls):
+        return ThreefoldBonusNetworkType.TESTNET
+
+    def minimum_miner_fee(self):
+        return Currency('0.1')
+
+    def default_explorer_addresses(self):
+        if self.__eq__(ThreefoldBonusNetworkType.TESTNET):
+            return [
+                'https://explorer.testnet.tfb.threefold.tech',
+            ]
+        if self.__eq__(ThreefoldBonusNetworkType.DEVNET):
+            return [
+                'http://localhost:21110'
+            ]
+        raise ValueError("invalid ThreeFoldBonus network type {}".format(self.value))
+
+ThreefoldBonusNetworkType.TESTNET = ThreefoldBonusNetworkType(1)
+ThreefoldBonusNetworkType.DEVNET = ThreefoldBonusNetworkType(2)
+
+
+class FreeFlowTokenNetworkType(NetworkType):
+    def __init__(self, value):
+        if isinstance(value, FreeFlowTokenNetworkType):
+            value = value.value
+        elif isinstance(value, str):
+            value = FreeFlowTokenNetworkType.from_str(value).value
+        if not isinstance(value, int):
+            raise TypeError("FreeFlowToken network type value was expected to be an int, not be of type {}".format(type(value)))
+        if value < 1 or value > 2:
+            raise ValueError("FreeFlowToken network type out of range: {}".format(value))
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def __eq__(self, other):
+        if isinstance(other, FreeFlowTokenNetworkType):
+            return self.value == other.value
+        return self.value == other
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __int__(self):
+        return self.value
+
+    def __str__(self):
+        if self.__eq__(FreeFlowTokenNetworkType.TESTNET):
+            return "testnet"
+        if self.__eq__(FreeFlowTokenNetworkType.DEVNET):
+            return "devnet"
+        raise ValueError("invalid FreeFlowToken network type {}".format(self.value))
+
+    @classmethod
+    def from_str(cls, s):
+        if not isinstance(s, str):
+            raise TypeError("can only convert from a string")
+        s = jsstr.lower(s)
+        if s in ("testnet", "test"):
+            return FreeFlowTokenNetworkType.TESTNET
+        if s in ("devnet", "dev"):
+            return FreeFlowTokenNetworkType.DEVNET
+        raise ValueError(s + " is not a supported FreeFlowToken network type str")
+
+    @classmethod
+    def default_network_type(cls):
+        return FreeFlowTokenNetworkType.TESTNET
+
+    def minimum_miner_fee(self):
+        return Currency('0.1')
+
+    def default_explorer_addresses(self):
+        if self.__eq__(FreeFlowTokenNetworkType.TESTNET):
+            return [
+                'https://explorer.testnet.fft.threefold.tech',
+            ]
+        if self.__eq__(FreeFlowTokenNetworkType.DEVNET):
+            return [
+                'http://localhost:22311'
+            ]
+        raise ValueError("invalid FreeFlowToken network type {}".format(self.value))
+
+FreeFlowTokenNetworkType.TESTNET = FreeFlowTokenNetworkType(1)
+FreeFlowTokenNetworkType.DEVNET = FreeFlowTokenNetworkType(2)
+
 # Chain Type
 
 class Type:
@@ -174,7 +306,7 @@ class Type:
             value = Type.from_str(value).value
         if not isinstance(value, int):
             raise TypeError("chain type value was expected to be an int, not be of type {}".format(type(value)))
-        if value < 1 or value > 2:
+        if value < 1 or value > 4:
             raise ValueError("chain type out of range: {}".format(value))
         self._value = value
 
@@ -197,6 +329,10 @@ class Type:
             return "tfchain"
         if self.__eq__(Type.GOLDCHAIN):
             return "goldchain"
+        if self.__eq__(Type.TFBCHAIN):
+            return "tfbchain"
+        if self.__eq__(Type.FFTCHAIN):
+            return "fftchain"
         raise ValueError("invalid network type {}".format(self.value))
 
     def str(self):
@@ -211,6 +347,10 @@ class Type:
             return Type.TFCHAIN
         if s == "goldchain":
             return Type.GOLDCHAIN
+        if s == "tfbchain":
+            return Type.TFBCHAIN
+        if s == "fftchain":
+            return Type.FFTCHAIN
         raise ValueError(s + " is not a supported chain type str")
 
     def network_type(self):
@@ -218,6 +358,10 @@ class Type:
             return TFChainNetworkType
         if self.__eq__(Type.GOLDCHAIN):
             return GoldChainNetworkType
+        if self.__eq__(Type.TFBCHAIN):
+            return ThreefoldBonusNetworkType
+        if self.__eq__(Type.FFTCHAIN):
+            return FreeFlowTokenNetworkType
         raise ValueError("invalid network type {}".format(self.value))
 
     def currency_unit(self):
@@ -225,7 +369,16 @@ class Type:
             return "TFT"
         if self.__eq__(Type.GOLDCHAIN):
             return "GFT"
+        if self.__eq__(Type.TFBCHAIN):
+            return "TFB"
+        if self.__eq__(Type.FFTCHAIN):
+            return "FFT"
         raise ValueError("invalid network type {}".format(self.value))
 
 Type.TFCHAIN = Type(1)
 Type.GOLDCHAIN = Type(2)
+Type.TFBCHAIN = Type(3)
+Type.FFTCHAIN = Type(4)
+
+Type._MIN_CHAIN_TYPE = Type.TFCHAIN
+Type._MAX_CHAIN_TYPE = Type.FFTCHAIN
