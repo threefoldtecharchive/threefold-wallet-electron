da5bb5f2efe3282ddb341a7cbfafeb61a176b152
Glen De Cauwsemaecker <contact@glendc.com>
2019-11-25 17:07:22 +0100

    intigrate safe-eval in ScriptedTransaction

--- app/components/transactions/ScriptedTransaction.js
+++ app/components/transactions/ScriptedTransaction.js
@@ -1,10 +1,13 @@
 // @flow
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
-import { Form, Button, TextArea, Icon } from 'semantic-ui-react'
+import { Form, Button, TextArea, Icon, Dropdown } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import styles from '../home/Home.css'
+import { truncate, concat } from 'lodash'
+
+const safeEval = require('safe-eval')
 
 const TransactionTypes = {
   SINGLE: 'SINGLE',
@@ -23,7 +26,8 @@ class ScriptedTransaction extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      transactionType: TransactionTypes.SCRIPTED
+      transactionType: TransactionTypes.SCRIPTED,
+      selectedWallet: this.props.form.transactionForm.values.selectedWallet
     }
   }
 
@@ -32,16 +36,85 @@ class ScriptedTransaction extends Component {
   }
 
   sendScript = () => {
-    const { script } = this.state
-    console.log(script)
-    toast('yeeey you just wrote a script :) :) :) :)')
+    const { script, selectedWallet } = this.state
+    const context = {
+      wallet: selectedWallet
+    }
+    let ok = true
+    let result
+    try {
+      result = safeEval(script, context)
+    } catch (error) {
+      ok = false
+      console.log('error while evaluating script', error)
+      this.setState({ scriptOutput: 'Error: ' + error })
+    } finally {
+      if (ok) {
+        toast('script executed succesfully')
+        if (result && result.output) {
+          this.setState({ scriptOutput: result.output })
+        }
+      }
+    }
+  }
+
+  selectWallet = (address) => {
+    const wallet = this.props.account.wallet_for_address(address)
+    this.setState({ selectedWallet: wallet })
+    this.props.account.select_wallet({ name: wallet.wallet_name, address: wallet.address })
+  }
+
+  renderDropdownList = ({ input, data, valueField, textField }) => {
+    const { selectedWallet } = this.state
+    return (
+      <Form.Field>
+        <label>Select Wallet *</label>
+        <Dropdown
+          placeholder='Select wallet'
+          options={data}
+          value={selectedWallet.address}
+          fluid
+          selection
+          onChange={(e, v) => {
+            this.selectWallet(v.value)
+          }} />
+      </Form.Field>
+    )
+  }
+
+  mapWalletsToDropdownOption = () => {
+    if (this.props.account) {
+      let { wallets, multisig_wallets: multiSigWallets } = this.props.account
+
+      const nWallets = wallets.map(w => {
+        return {
+          key: `NM: ${w.wallet_name}`,
+          text: w.wallet_name,
+          value: w.address
+        }
+      })
+
+      const mWallets = multiSigWallets.map(w => {
+        const id = w.wallet_name || w.address
+        return {
+          key: `MS: ${id}`,
+          text: `Multisig: ${truncate(id, { length: 24 })}`,
+          value: w.address
+        }
+      })
+
+      const newWallets = concat(nWallets, mWallets)
+      return newWallets
+    }
   }
 
   render () {
-    const { script } = this.state
+    const { script, scriptOutput } = this.state
+    const walletOptions = this.mapWalletsToDropdownOption()
 
     return (
       <Form>
+        {this.renderDropdownList({ data: walletOptions })}
         <Form.Field>
           <TextArea
             style={{ background: '#0c111d !important', color: '#7784a9', height: 250 }}
@@ -51,7 +124,10 @@ class ScriptedTransaction extends Component {
             onChange={this.handleScriptChange}
           />
         </Form.Field>
-        <div style={{ float: 'right' }}>
+        {scriptOutput && <TextArea
+          value={scriptOutput}
+        />}
+        <div style={{ float: 'right', marginTop: 10 }}>
           <Button className={styles.cancelButton} onClick={() => this.props.history.goBack()} > Cancel </Button>
           <Button className={styles.acceptButton} onClick={() => this.sendScript()} >Send</Button>
         </div>
